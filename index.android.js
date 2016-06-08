1ae5f730616ca97507cb447d9f79a5e2e15d85a1
252419 <422025233@qq.com>
2016-06-08 18:18:56 +0800

    add index.android.js

---
+++ index.android.js
@@ -0,0 +1,16 @@
+import React, {
+  AppRegistry,
+  Component
+} from 'react-native';
+
+import Main from './app/components/main';
+
+let ReactNativeTodos = React.createClass({
+  render() {
+    return (
+      <Main />
+    );
+  }
+});
+
+AppRegistry.registerComponent('ReactNativeRocketChat', () => ReactNativeRocketChat);
